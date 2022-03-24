2a66b868371f5039b4c5121a5c6024fe8ae67e93
koosh4 <koosha.owlad@gmail.com>
2022-03-24 18:38:53 +0100

    app shows deleted numbers

--- duplicate_number.py
+++ duplicate_number.py
@@ -1,5 +1,10 @@
 numbers = [5,8,2,3,12,4,9,3,18,8,10,6,10]
+numbers_copy = numbers.copy()
+duplicates = []
 for X in numbers:
     if numbers.count(X) == 2:
+        duplicates.append((X))
         numbers.remove(X)
-print(numbers)
+print(f"The old list is: {numbers_copy}")
+print(f"New list without duplicates is: {numbers}")
+print(f"Deleted numbers: {duplicates}")
