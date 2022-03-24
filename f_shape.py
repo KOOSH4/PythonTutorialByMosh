eeb6171ae88992e2d4e5e37e2c8afc8fa7ab2616
koosh4 <koosha.owlad@gmail.com>
2022-03-24 17:12:03 +0100

    new solution added

--- f_shape.py
+++ f_shape.py
@@ -2,3 +2,12 @@ for word_4X in range(2):
     print("XXXXX")
     for word_2X in range(word_4X+1):
         print("XX")
+
+print("\n")
+
+numbers = [5,2,5,2,2]
+for i in numbers:
+    Word = ''
+    for k in range(i):
+        Word += 'X'
+    print(Word)
