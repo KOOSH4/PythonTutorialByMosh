b11d41d1f673c7e66c440225fce1f9f6b2a48425
koosh4 <koosha.owlad@gmail.com>
2022-03-24 19:20:24 +0100

    Translate number solved

---
+++ Translate_number.py
@@ -0,0 +1,15 @@
+Numbers = {
+    "1" : "one",
+    "2" : "two",
+    "3" : "three",
+    "4" : "four",
+    "5" : "five",
+    "6" : "six",
+    "7" : "seven",
+    "8" : "eight",
+    "9" : "nine",
+    "0" : "zero"
+}
+user_Input = input("Enter a number (at least 3 digit): ")
+for X in str(user_Input):
+    print(Numbers[X])
